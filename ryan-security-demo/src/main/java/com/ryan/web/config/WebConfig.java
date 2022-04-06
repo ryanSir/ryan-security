5bf3840f5f8ce7ea8bfdc552496ac2c504b74abc
Ryan <yang.zhao@oceanus-plus.com>
2022-04-06 16:37:58 +0800

    拦截器

--- ryan-security-demo/src/main/java/com/ryan/web/config/WebConfig.java
+++ ryan-security-demo/src/main/java/com/ryan/web/config/WebConfig.java
@@ -1,9 +1,13 @@
 package com.ryan.web.config;
 
 import com.ryan.web.filter.TimeFilter;
+import com.ryan.web.interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +17,16 @@ import java.util.List;
  * @version Id: WebConfig, v 0.1 2022/4/6 4:07 PM ryan Exp $
  */
 @Configuration
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor);
+    }
 
     @Bean
     public FilterRegistrationBean timeFilter(){
---
+++ ryan-security-demo/src/main/java/com/ryan/web/interceptor/TimeInterceptor.java
@@ -0,0 +1,45 @@
+package com.ryan.web.interceptor;
+
+import org.springframework.boot.actuate.endpoint.jmx.DataEndpointMBean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+/**
+ * @author ryan
+ * @version Id: TimeInterceptor, v 0.1 2022/4/6 4:16 PM ryan Exp $
+ */
+@Component
+public class TimeInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handlerController) throws Exception {
+        System.out.println("preHandle");
+
+        System.out.println(((HandlerMethod) handlerController).getBean().getClass().getName());
+        System.out.println(((HandlerMethod) handlerController).getMethod().getName());
+
+        httpServletRequest.setAttribute("startTime", new Date().getTime());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle");
+        Long start = (Long) httpServletRequest.getAttribute("startTime");
+        System.out.println("time interceptor 耗时：" + (new Date().getTime() - start));
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
+        System.out.println("afterHandle");
+        Long start = (Long) httpServletRequest.getAttribute("startTime");
+        System.out.println("time interceptor 耗时：" + (new Date().getTime() - start));
+
+    }
+}
